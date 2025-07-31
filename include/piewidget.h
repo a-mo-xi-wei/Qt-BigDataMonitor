18e99e83e89c91e10c7ce5b57e7eae8087f6f5d2
WeiWang <1428206861@qq.com>
2025-07-31 19:05:38 +0800

    完成

---
+++ include/piewidget.h
@@ -0,0 +1,36 @@
+﻿#ifndef PIEWIDGET_H
+#define PIEWIDGET_H
+
+#include <QWidget>
+#include <QList>
+#include <QPainterPath>
+
+#if _MSC_VER >= 1600
+#pragma execution_character_set("utf-8")
+#endif
+
+class CPieWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit CPieWidget(QWidget *parent = nullptr);
+
+    struct Item
+    {
+        qreal percent;
+        QString name;
+        QColor color;
+        QPainterPath path;
+    };
+
+    void append(qreal percent, QString name);
+
+protected:
+    void paintEvent(QPaintEvent *);
+    void resizeEvent(QResizeEvent *event);
+
+private:
+    QList<Item> m_listItem;
+};
+
+#endif // PIEWIDGET_H
---
+++ run_with_qt5.bat
@@ -0,0 +1,5 @@
+@echo off
+setlocal
+set PATH=F:\Qt\5.15.2\mingw81_64\bin;%PATH%
+start cmake-build-release-mingw-qt-5_15_2\widget.exe
+endlocal
